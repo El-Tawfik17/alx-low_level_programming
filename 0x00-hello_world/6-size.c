f6cdbdb52c3fe2f1dc2e8dc11b1adbe4e4b89edd
Tawfik <kifwat17@gmail.com>
2022-07-06 11:29:15 -0700

    update new5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,7 @@
  *this program prints "Progamming is like a multilingual puzzle"
  *Return:0
  */
+
 int main(void) 
 {
 char c;
@@ -17,5 +18,5 @@ printf("Size of an int: %ld byte(s)\n", sizeof(i));
 printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 printf("Size of a float: %ld byte(s)\n", sizeof(f));
-return(0);
+return (0);
 }
