c8d46467016ce6878309a10b4ed598be2f2c9e75
Tawfik <kifwat17@gmail.com>
2022-07-07 11:38:46 -0700

    last commit after correct betty style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,11 +5,11 @@
  */
 void print_alphabet_x10(void)
 {
-	char i,c;
+	char i, c;
 
-	for( i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for(c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
