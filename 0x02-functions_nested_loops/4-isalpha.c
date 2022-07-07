f99d42dfe9e387a2ee406c11c380b878dfebb3f9
Tawfik <kifwat17@gmail.com>
2022-07-07 12:18:23 -0700

    I write _isalpha function

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islpha-function check if is letter and lowercase or uppercase
+ * Description:this function is write return one or zero
+ * @c:is paramater for our function
+ * Return:0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (0);
+	}
+	else
+		return (0);
+}
