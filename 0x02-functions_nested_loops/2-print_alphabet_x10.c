1a40a6ba2e69e9941902d9b9285a71cd9601427b
Tawfik <kifwat17@gmail.com>
2022-07-07 11:29:58 -0700

    i write function that repeat ten time the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10-this function print ten times the alphabet in lowercase
+ * Description:This funtion will run print_alphabet ten time
+ */
+void print_alphabet_x10(void)
+{
+	char  i , c;
+	for ( i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+	_putchar('\n');
+
+	}
+}
+
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define _ALX_
 int _putchar(char);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 #endif
