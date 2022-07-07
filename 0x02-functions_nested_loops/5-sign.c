bb1f531d81051dc255fb6c297d9f70fd22f41d26
Tawfik <kifwat17@gmail.com>
2022-07-07 12:42:39 -0700

    I write print_sign function

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.c"
+/**
+ * print_sign(int n) - function print the sign of number
+ * @n:is paramater for the function
+ * Return:0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+	return (0);
+}
