6c2e63c4d4c740762c19354562b4c110cab6a950
Tawfik <kifwat17@gmail.com>
2022-07-07 12:06:41 -0700

    I make description for islower function

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,12 @@
 /**
  * _islower-checker the lower character
  * Description:This function checker if the character is lower
+ * @c:is parameter for our function islower
  * Return:0
  */
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
