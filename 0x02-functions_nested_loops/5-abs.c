4a6636b1f71a7fdd0a5793d70c65a04d4e416a2a
Tawfik <kifwat17@gmail.com>
2022-07-07 13:27:25 -0700

    I write abs function

---
+++ 0x02-functions_nested_loops/5-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs -function computes the absolute value
+ * @n:is paramater
+ * Return:0
+ */
+int _abs(int n)
+{
+	if ( n > 0 || c == 0)
+	{
+		return (n);
+	}
+	else
+		return (n*-1);
+}
+
