a9ad10a29945444252febe89dffca49ad131b163
Tawfik <kifwat17@gmail.com>
2022-07-08 12:39:07 -0700

    print last digital

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - function return the digit number of the last nomber
+ * @c:is paramater for the function
+ * Return:0
+ */
+int print_last_digit(int c)
+{
+	if (c > 0 || c == 0)
+	{
+		_putchar(c % 10 + '0');
+		return (c % 10);
+	}
+	else
+	{
+		c = c * -1;
+		_putchar(c % 10 + '0');
+		return (c % 10);
+	}
+}	
