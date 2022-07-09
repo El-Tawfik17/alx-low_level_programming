0013c17cce2f53b43b73d38b28ce35c669d3a8d7
Tawfik <kifwat17@gmail.com>
2022-07-09 09:52:56 -0700

    new day for commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,40 +1,34 @@
 #include "main.h"
 /**
- * times_table - prints the table of an interger starting with zero
+ * times_table - function print nine times tables
  * Return:0
  */
-void times_tables(void)
+void times_table(void)
 {
-	int x,y,pr;
-
+	int x, y, z;
 	for (x = 0; x <= 9; x++)
 	{
-		for (y=0; y <= 9; y++)
-		{
-		pr = x * y;
-		
-		if (y != 0)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		if (y == 0)
-		{
-			_putchar('0');
-		}
-		else if (pr >= 10)
-		{
-			_putchar((pr / 10) + '0');
-			_putchar((pr % 10) + '0');
-		}
-		else if ((pr < 10) && (y != 0))
-		{
-			_putchar(' ');
-			_putchar((pr % 10) + '0');
-		}
-		}
-		_putchar('\n');
+	_putchar('0');
+	_putchar(',');
+	_putchar(' ');
+	for (y = 1; y <= 9; y++)
+	{
+	d= (x * y);
+	if ((d / 10) > 0)
+	{
+	_putchar((d / 10) + '0');
 	}
-}
-
-
+	else
+	{
+	_putchar(' ');
+	}
+	_putchar((d % 10) + '0');
+	if (y < 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	}
+	_putchar('\n');
+	}
+}	
