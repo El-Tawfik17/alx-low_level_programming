84110a5ba259b1e4e867248e7044e267986357ef
Tawfik <kifwat17@gmail.com>
2022-07-09 12:53:26 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry
+ * Return:0
+ */
+int main(void)
+{
+	char alpha = 'a';
+	
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
