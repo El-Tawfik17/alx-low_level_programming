e9cdeaa995436d90a7c069e8107a7c53d724283a
Tawfik <kifwat17@gmail.com>
2022-07-09 13:29:43 -0700

    print number

---
+++ 0x01-variables_if_else_while/5-print_number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return:0
+ */
+int main(void)
+{
+	char num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
