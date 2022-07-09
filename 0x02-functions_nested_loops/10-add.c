4b857b2ef64f0cddaf850237fbefff27c6230014
Tawfik <kifwat17@gmail.com>
2022-07-09 10:11:31 -0700

    add function were wrote by Tawfik

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function print the adding of two integer
+ * @a:paramater of function
+ * @b:paramater of function
+ * Return:0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
