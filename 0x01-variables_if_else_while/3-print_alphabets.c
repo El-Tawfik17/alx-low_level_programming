28490c3c984c53ba131665f3f44e4098e5c6869e
Tawfik <kifwat17@gmail.com>
2022-07-09 13:07:41 -0700

    upper and lower programme

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return:0
+ */
+int main(void)
+{
+	char lower = 'a', upper = 'A';
+
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
+	putchar('\n');
+	return (0);
+}
