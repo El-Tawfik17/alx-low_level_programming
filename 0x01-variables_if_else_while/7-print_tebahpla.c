7b1967edd10ad921e23b293e3cd81c05133d658f
Tawfik <kifwat17@gmail.com>
2022-07-09 14:02:13 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return:0
+ */
+int main(void)
+{
+	char al = 'z';
+
+	while (al >= 'a')
+	{
+		putchar(al);
+		al++;
+	}
+	putchar('\n');
+	return (0);
+}
