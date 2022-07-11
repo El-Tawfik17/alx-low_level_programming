100f2b4d7ed085c0eef9f596cf0a1dce3a783f70
Tawfik <kifwat17@gmail.com>
2022-07-11 13:25:37 -0700

    puts function

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,2 +1,17 @@
 #include "main.h"
-
+/**
+ * puts2 - function 
+ * @str: is paramater
+ * Return:0
+ */
+void puts2(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+}
