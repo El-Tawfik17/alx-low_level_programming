a7342b3218e50e1fe5a7d49396d16773a574bc4b
Tawfik <kifwat17@gmail.com>
2022-07-11 12:32:19 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - function reverse a string
+ * @s:isparamater
+ * Return:0
+ */
+void rev_string(char*s)
+{
+	int i = 0, x, y, c;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	x = 0;
+	y = i - 1;
+	while (y >= 0)
+	{
+		c = s[x];
+		s[x] = s[y];
+		s[y] = c;
+		x++;
+		y--;
+	}
+}
